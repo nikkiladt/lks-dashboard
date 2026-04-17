81bcfc4f45afd928135e3eddd87aee778159904c
nikkiladt <nikkiladt@lksandassociates.com>
2026-04-17 08:53:34 -0600

    Patch
    {"error":"Unauthorized"}

--- auth.ts
+++ auth.ts
@@ -2,6 +2,9 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID!,
