84fa027f2669ddccc3833be7ccd7963d4beee7e3
nikkiladt <nikkiladt@lksandassociates.com>
2026-04-17 09:26:13 -0600

    patch

--- auth.ts
+++ auth.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
   session: {
     strategy: "jwt",
   },
